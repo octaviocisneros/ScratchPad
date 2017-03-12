b1c9c57c746c1bbcfd6813caf50157a95a9afaef
Octavio Cisneros <octavio@Octavios-MacBook-Air.local>
2017-03-12 11:57:53 -0400

    FinalVersion

---
+++ src/com/theironyard/novauc/Array1Test.java
@@ -0,0 +1,28 @@
+package com.theironyard.novauc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by octavio on 3/12/17.
+ */
+public class Array1Test {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void unlucky1() throws Exception {
+
+    }
+
+}
---
+++ src/com/theironyard/novauc/Logic111Test.java
@@ -0,0 +1,28 @@
+package com.theironyard.novauc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by octavio on 3/12/17.
+ */
+public class Logic111Test {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void shareDigit() throws Exception {
+
+    }
+
+}
